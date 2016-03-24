6bc24a9145e13628ae71098e223b47a2a8ba2ca6
Jason Duffy <jasonsduffy@gmail.com>
2016-03-23 21:11:03 -0400

    Initial Commit

---
+++ BankOCR/BankOCR.h
@@ -0,0 +1,19 @@
+//
+//  BankOCR.h
+//  BankOCR
+//
+//  Created by Jason Duffy on 3/23/16.
+//  Copyright © 2016 Kata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BankOCR.
+FOUNDATION_EXPORT double BankOCRVersionNumber;
+
+//! Project version string for BankOCR.
+FOUNDATION_EXPORT const unsigned char BankOCRVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BankOCR/PublicHeader.h>
+
+
